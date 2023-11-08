2372480d95cfb33f2bea48ee3876ec10a404c631
Nencold <soulbeerman@gmail.com>
2023-11-08 19:11:17 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
-import Slider from './Slider/Slider';
+import React from 'react';
+import SliderComponent from './Slider/Slider';
 
-function App() {
+import './App.css';
+
+
+const App = () => {
+  const slides = [
+    'images/1200x480-howling-wolf-3xvjis1804fz4v5j.jpg',
+    'images/1200x480-sakura-tree-3lorr5btufgdi6pj.jpg',
+    'images/1200x480-vaporwave-mountains-e1fotmpsfib9vp80.jpg',
+    'images/1625170782_37-kartinkin-com-p-fon-dlya-tvicha-krasivie-foni-38.jpg',
+    'images/1200x480-pink-eclipse-8oc867idlp81x9ox.jpg'
+
+  ];
 
   return (
-    <div className="container">
-      <Slider>
-        
-      </Slider>
+    <div>
+      <SliderComponent slides={slides} />
     </div>
   );
-}
+};
 
 export default App;
+
--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -1,85 +1,30 @@
-import React, {useEffect, useRef, useState} from "react";
-import './Slider.css';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-export default function Slider(){
-    const sliderRef = useRef();
-    const sliderLineRef = useRef();
-    const [width, setWidth] = useState();
-    const [offset, setOffset] = useState(0);
+import './Slider.scss';
 
-    let sliderImages = [
-        {
-            id: 0,
-            src: "images/header1440.png",
-        },
-        {
-            id: 1,
-            src: "images/header1440.png"
-        },
-        {
-            id: 2,
-            src: "images/header1440.png"
-        },
-        {
-            id: 3,
-            src: "images/header1440.png"
-        },
-        {
-            id: 4,
-            src: "images/header1440.png"
-        },
-    ]
-    
-    useEffect(() => {
-        setWidth(sliderRef.current.offsetWidth)
-        const resizeHandler = () =>{
-            let _width = sliderRef.current;
-            setWidth(_width.offsetWidth)    
-            setOffset(0)
-        }
-        resizeHandler()
-        window.addEventListener('resize', resizeHandler)
-    },[])
+const SliderComponent = ({ slides }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-    const handleRightClick = () => {
-        setOffset((currentOffset) => {
-            const newOffset = currentOffset - width;
-            const maxWidth = -(width * sliderImages.length);
-            if(newOffset <= maxWidth){
-                return 0;
-            } else{
-                return Math.max(newOffset);
-            }
-        })
-    }
+  return (
+    <div className='Slider'>
+      <Slider {...settings}>
+        {slides.map((slide, index) => (
+          <div key={index}>
+            <img src={slide} alt={`Slide ${index}`} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
-    const handleLeftClick = () => {
-        setOffset((currentOffset) => {
-            const newOffset = currentOffset + width;
-            if(newOffset <= 0){
-                return Math.min(newOffset);
-            } else{
-                return Math.min(-(width * (sliderImages.length-1)))
-            }
-        })
-    }
-    
-    
-    return(
-        <div className="slider-content">
-            {console.log(1)}
-            <div className="slider" ref={sliderRef}>
-                <div className="slider-line" ref={sliderLineRef} style={{width: width * sliderImages.length + 'px', transform:`translateX(${offset}px)`}}>
-                    
-                    {sliderImages.map((item, id) => {
-                        return(
-                        <img src={item.src} style={{width:width, height:"auto"}} alt={id} key={item.id + id}/>
-                        )
-                    })}
-                </div>
-            </div>
-            <button className="slider-left" onClick={handleLeftClick}>left</button>
-            <button className="slider-right" onClick={handleRightClick}>right</button>
-        </div>
-    );
-}
+export default SliderComponent;
